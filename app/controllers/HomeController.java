a4597042edc868eb44dc8bd8f33f2f0ea66791df
fmauri <mauri95francesco@gmail.com>
2017-10-26 12:31:38 +0200

    route modification to satisfy heroku needs

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -18,11 +18,13 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+
         return ok(views.html.index.render());
     }
 
 
     public Result staticFile(String path) {
+
         return ok(new java.io.File("public/index.html"));
     }
 
