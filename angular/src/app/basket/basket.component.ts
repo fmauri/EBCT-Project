5cbf0ad73e04e9bd9999846a58d28c2bee0bf063
Wojciech Rauk <wojtek.rauk@gmail.com>
2017-11-13 22:32:19 +0100

    paypal and angular integration

--- angular/src/app/basket/basket.component.ts
+++ angular/src/app/basket/basket.component.ts
@@ -6,6 +6,8 @@ import { ProductsService } from './../products.service';
 import { BasketService } from './../basket-service.service';
 import { Product } from '../product.model';
 
+declare const paypal: any;
+
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
@@ -32,7 +34,61 @@ export class BasketComponent implements OnInit {
     this.basketProducts = this.basketService.getBasketProducts();
     this.basketQuantity = this.basketService.getBasketProducts().length;
     this.basketTotal = this.basketService.getBasketTotal();
+    this.initPaypal();
+  }
 
+  initPaypal(){
+    const self = this;
+    paypal.Button.render({
+
+      env: 'sandbox', // Or 'sandbox',
+
+      client: {
+        sandbox: 'ActHlZqElF4vSwYPMiBd5Ww-TnMEtxbl8hCHDNjvNaN2jYTJ31-se_T_SGF2yjzqcn_8Xm3gauV9fPtI',
+        production: 'AaMVs1HrduBk2BxcLmw2yUI5qJ5IaikjGtBkFphlq1NUwWwG9og4-X2Wat6AGfnmW1o58RIlP-jNYmaz'
+      },
+
+      commit: true, // Show a 'Pay Now' button
+
+      style: {
+        color: 'gold',
+        size: 'small'
+      },
+
+      payment: function (data, actions) {
+        // todo - hide button when basketTotal is zero? make it inactive?
+        return actions.payment.create({
+          payment: {
+            transactions: [
+              {
+                amount: {total: self.basketTotal, currency: 'PLN'}
+              }
+            ]
+          }
+        });
+      },
+
+      onAuthorize: function (data, actions) {
+        return actions.payment.execute().then(function(payment) {
+
+          // The payment is complete!
+          // You can now show a confirmation message to the customer
+        });
+      },
+
+      onCancel: function (data, actions) {
+        /*
+         * Buyer cancelled the payment
+         */
+      },
+
+      onError: function (err) {
+        /*
+         * An error occurred during the transaction
+         */
+      }
+
+    }, '#paypal-button');
   }
 
   onEmptyBasket() {
